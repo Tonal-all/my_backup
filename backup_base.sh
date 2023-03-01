a5045db4ada3bde1deb4931704a825ed29ba2b07
Tonal-all <1756222648@qq.com>
2023-03-01 19:34:43 +0800

    update configration

--- backup_base.sh
+++ backup_base.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 cd ~/.backup/base_cofigration
-cp ~/.vim ~/.vimrc ~/.bashrc ~/.profile .
+cp ~/.vim ~/.vimrc ~/.bashrc ~/.profile ~/.config/nvim .
 git add .
 git commit -m "update configration"
 git push origin
